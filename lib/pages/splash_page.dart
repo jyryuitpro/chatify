3a872a5559cc0e6317db41a96703dd72e10d1090
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-11-06 20:21:57 +0900

    Create Splash Page

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  final VoidCallback onInitializationComplete;
+
+  const SplashPage({required Key? key, required this.onInitializationComplete})
+      : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chatify',
+      theme: ThemeData(
+        backgroundColor: Color.fromRGBO(36, 35, 49, 1.0),
+        scaffoldBackgroundColor: Color.fromRGBO(36, 35, 49, 1.0),
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.contain,
+                image: AssetImage('assets/images/logo.png'),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
