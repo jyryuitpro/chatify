853d19dc36238a1068ab600886e7c46f103fb0d4
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-11-06 22:55:57 +0900

    Adding Page Title

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -8,14 +8,46 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  late double _deviceHeight;
+  late double _deviceWidth;
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
     return _buildUI();
   }
 
   Widget _buildUI() {
     return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: _deviceWidth * 0.03,
+          vertical: _deviceHeight * 0.02,
+        ),
+        height: _deviceHeight * 0.98,
+        width: _deviceWidth * 0.97,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _pageTitle(),
+          ],
+        ),
+      ),
+    );
+  }
 
+  Widget _pageTitle() {
+    return Container(
+      height: _deviceHeight * 0.10,
+      child: Text(
+        'Chatify',
+        style: TextStyle(
+            color: Colors.white, fontSize: 40, fontWeight: FontWeight.w600),
+      ),
     );
   }
 }
