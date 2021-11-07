9cbe0e1dc379c03e02439dca3ad225d5725eb8c6
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-11-07 12:29:40 +0900

    Parsing User Data

--- lib/providers/authentication_provider.dart
+++ lib/providers/authentication_provider.dart
@@ -1,3 +1,4 @@
+import 'package:chatify/models/chat_user.dart';
 import 'package:chatify/services/database_service.dart';
 import 'package:chatify/services/navigation_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -9,14 +10,32 @@ class AuthenticationProvider extends ChangeNotifier {
   late final NavigationService _navigationService;
   late final DatabaseService _databaseService;
 
+  late ChatUser user;
+
   AuthenticationProvider() {
     _auth = FirebaseAuth.instance;
     _navigationService = GetIt.instance.get<NavigationService>();
     _databaseService = GetIt.instance.get<DatabaseService>();
     _auth.authStateChanges().listen((_user) {
-      if(_user != null) {
+      if (_user != null) {
         _databaseService.updateUserLastSeenTime(_user.uid);
-        _databaseService.getUser(_user.uid);
+        _databaseService.getUser(_user.uid).then(
+          (_snapshot) {
+            Map<String, dynamic> _userData =
+                _snapshot.data() as Map<String, dynamic>;
+            user = ChatUser.fromJSON(
+              {
+                "uid": _user.uid,
+                "name": _userData["name"],
+                "email": _userData["email"],
+                "image": _userData["image"],
+                "last_active": _userData["last_active"],
+              },
+            );
+            print(user);
+            print(user.toMap());
+          },
+        );
       } else {
         print('Not Authenticated');
       }
@@ -26,11 +45,12 @@ class AuthenticationProvider extends ChangeNotifier {
   Future<void> loginUsingEmailAndPassword(
       String _email, String _password) async {
     try {
-      await _auth.signInWithEmailAndPassword(email: _email, password: _password);
+      await _auth.signInWithEmailAndPassword(
+          email: _email, password: _password);
       print(_auth.currentUser);
     } on FirebaseAuthException catch (e) {
       print('Error loggin user into Firebase');
-    } catch(e) {
+    } catch (e) {
       print(e);
     }
   }
