ebda4da47416d8a3481b141b13dfa54ffa593f83
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-11-07 23:28:33 +0900

    Writing Firebase Cloud Storage Function

--- lib/services/cloud_storage_service.dart
+++ lib/services/cloud_storage_service.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 const String USER_COLLECTION = 'Users';
@@ -6,4 +10,32 @@ class CloudStorageService {
   final FirebaseStorage _storage = FirebaseStorage.instance;
 
   CloudStorageService();
+
+  Future<String?> saveUserImageToStorage(
+      String _uid, PlatformFile _file) async {
+    try {
+      Reference _ref =
+          _storage.ref().child('images/users/$_uid/profile.${_file.extension}');
+      UploadTask _task = _ref.putFile(
+        File(_file.path!),
+      );
+      return await _task.then((_result) => _result.ref.getDownloadURL());
+    } on Exception catch (e) {
+      print(e);
+    }
+  }
+
+  Future<String?> saveChatImageToStorage(
+      String _chatID, String _userID, PlatformFile _file) async {
+    try {
+      Reference _ref = _storage.ref().child(
+          'images/chats/$_chatID/${_userID}_${Timestamp.now().millisecondsSinceEpoch}.${_file.extension}');
+      UploadTask _task = _ref.putFile(
+        File(_file.path!),
+      );
+      return await _task.then((_result) => _result.ref.getDownloadURL());
+    } on Exception catch (e) {
+      print(e);
+    }
+  }
 }
