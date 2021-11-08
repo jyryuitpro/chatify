b3fc81da5f68936b2e292d2c8ffb8447c7bde870
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-11-08 14:57:59 +0900

    Creating Chat Message Model

--- lib/pages/chats_page.dart
+++ lib/pages/chats_page.dart
@@ -1,4 +1,7 @@
+import 'package:chatify/providers/authentication_provider.dart';
+import 'package:chatify/widgets/top_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ChatsPage extends StatefulWidget {
   const ChatsPage({Key? key}) : super(key: key);
@@ -8,14 +11,45 @@ class ChatsPage extends StatefulWidget {
 }
 
 class _ChatsPageState extends State<ChatsPage> {
+  late double _deviceHeight;
+  late double _deviceWidth;
+
+  late AuthenticationProvider _auth;
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    _auth = Provider.of<AuthenticationProvider>(context);
+
     return _buildUI();
   }
 
   Widget _buildUI() {
-    return Scaffold(
-      backgroundColor: Colors.red,
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: _deviceWidth * 0.03,
+        vertical: _deviceHeight * 0.02,
+      ),
+      height: _deviceHeight * 0.98,
+      width: _deviceWidth * 0.97,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TopBar(
+            'Chats',
+            primaryAction: IconButton(
+              icon: Icon(Icons.logout),
+              color: Color.fromRGBO(0, 82, 218, 1.0),
+              onPressed: () {
+                _auth.logout();
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
