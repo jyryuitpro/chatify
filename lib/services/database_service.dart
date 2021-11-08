c33fdaf92b58ac6a1d8690d2e03e5e6628982d1f
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-11-08 21:27:15 +0900

    Get Last Message For Chat Chats From Firestore

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 const String USER_COLLECTION = 'Users';
 const String CHAT_COLLECTION = 'Chats';
-const String MESSAGES_COLLECTION = 'Messages';
+const String MESSAGES_COLLECTION = 'messages';
 
 class DatabaseService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
@@ -31,6 +31,16 @@ class DatabaseService {
         .collection(CHAT_COLLECTION)
         .where('members', arrayContains: _uid)
         .snapshots();
+  }
+
+  Future<QuerySnapshot> getLastMessageForChat(String _chatID) {
+    return _db
+        .collection(CHAT_COLLECTION)
+        .doc(_chatID)
+        .collection(MESSAGES_COLLECTION)
+        .orderBy("sent_time", descending: true)
+        .limit(1)
+        .get();
   }
 
   Future<void> updateUserLastSeenTime(String _uid) async {
