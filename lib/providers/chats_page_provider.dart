e810a75ff033a9f7fc3e9f1d1ea9f5d093e31565
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-11-08 22:32:16 +0900

    * Setting up Chats Stream From Firestore For Firebase User

--- lib/providers/chats_page_provider.dart
+++ lib/providers/chats_page_provider.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 
 import 'package:chatify/models/chat.dart';
+import 'package:chatify/models/chat_message.dart';
+import 'package:chatify/models/chat_user.dart';
 import 'package:chatify/providers/authentication_provider.dart';
 import 'package:chatify/services/database_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
@@ -27,6 +30,51 @@ class ChatsPageProvider extends ChangeNotifier {
   }
 
   void getChats() async {
-
+    try {
+      _chatsStream =
+          _db.getChatsForUser(_auth.user.uid).listen((_snapshot) async {
+        chats = await Future.wait(
+          _snapshot.docs.map(
+            (_d) async {
+              Map<String, dynamic> _chatData =
+                  _d.data() as Map<String, dynamic>;
+              // Get Users In Chat
+              List<ChatUser> _members = [];
+              for (var _uid in _chatData["members"]) {
+                DocumentSnapshot _userSnapshot = await _db.getUser(_uid);
+                Map<String, dynamic> _userData =
+                    _userSnapshot.data() as Map<String, dynamic>;
+                _members.add(
+                  ChatUser.fromJSON(_userData),
+                );
+              }
+              // Get Last Message For Chat
+              List<ChatMessage> _messages = [];
+              QuerySnapshot _chatMessage =
+                  await _db.getLastMessageForChat(_d.id);
+              if (_chatMessage.docs.isNotEmpty) {
+                Map<String, dynamic> _messageData =
+                    _chatMessage.docs.first.data()! as Map<String, dynamic>;
+                ChatMessage _message = ChatMessage.fromJSON(_messageData);
+                _messages.add(_message);
+              }
+              // Return Chat Instance
+              return Chat(
+                uid: _d.id,
+                currentUserUid: _auth.user.uid,
+                activity: _chatData["is_activity"],
+                group: _chatData["is_group"],
+                members: _members,
+                messages: _messages,
+              );
+            },
+          ).toList(),
+        );
+        notifyListeners();
+      });
+    } on Exception catch (e) {
+      print('Error getting chats.');
+      print(e);
+    }
   }
 }
